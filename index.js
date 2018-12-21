2c1014c990dc465b37107c18e57d1f66573a7308
Krishel Dela Cruz <kdelacr91095@yahoo.com>
2018-12-21 12:36:20 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// calling an express function, using one single app 
+const app = express();
+
+
+//creating a route handeler 
+//req is the object representing the incoming request
+//res is the object representing the outgoing response
+app.get('/',(req, res) => {
+
+    res.send({hi: 'there'});
+});
+
+//in production
+const PORT = process.env.PORT || 5000;
+//instructs express to tell node that it wants to listen to the port 5000
+app.listen(5000);
