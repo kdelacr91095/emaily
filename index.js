02d4829d2aa46b8d6a3d107aafb6ee356332c994
Krishel Dela Cruz <kdelacr91095@yahoo.com>
2018-12-21 13:16:42 -0600

    changes

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.get('/',(req, res) => {
 //in production
 const PORT = process.env.PORT || 5000;
 //instructs express to tell node that it wants to listen to the port 5000
-app.listen(5000);
+app.listen(PORT);
